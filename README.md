9cfa622c759b72da1731c4916de07d9de06024bf
moisesmengo <moisesmengo@live.com>
2021-01-02 11:11:58 -0300

    criando loading e finalizando projeto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Gerador_de_Frases
+
+[Veja como ficou](https://moisesmengo.github.io/Gerador_de_Frases/)
--- script.js
+++ script.js
@@ -1,16 +1,69 @@
-// GET frase da API (frases em Ingles)
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
 
-async function getQuote(){
-    const proxyUrl =  'http://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
-    try {
-        const response = await fetch(proxyUrl + apiUrl)
-        const data = await response.json()
-        console.log(data)
-    } catch (error) {
-        getQuote()
-        console.log('Eitha, Não temos frase!' , error)
+//loading
+function loading(){
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+
+function complete(){
+    if(!loader.hidden){
+        quoteContainer.hidden = false
+        loader.hidden = true
     }
 }
 
+// GET frase da API (frases em Ingles)
+async function getQuote(){
+
+    loading()
+
+    const proxyUrl =  'http://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+
+    try {
+        const response = await fetch(proxyUrl + apiUrl)
+        const data = await response.json()
+
+        // Se não tiver autor na frase
+        if(data.quoteAuthor === ''){
+            authorText.innerText = 'Unknown'
+        }else{
+            authorText.innerText = data.quoteAuthor
+        }
+        
+        // Se a frase for muito longa
+        if(data.quoteText.length > 120){
+            quoteText.classList.add('long-quote')
+        }else{
+            quoteText.classList.remove('long-quote')
+        }
+        quoteText.innerText = data.quoteText
+
+        //stop loading
+
+        complete()
+
+    } catch (error) {
+        getQuote()
+    }
+}
+//Tweet quote
+function tweetQuote(){
+    const quote = quoteText.innerText
+    const author = authorText.innerText
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+    window.open(twitterUrl, '_blank')
+}
+
+// eventos
+newQuoteBtn.addEventListener('click', getQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+
+// on Load
 getQuote()
